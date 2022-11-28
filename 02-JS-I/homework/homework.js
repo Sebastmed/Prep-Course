cfc050785c485a8683322b20373c42c3452c7e6c
Sebastián <Usuario@Equipo-PC>
2022-11-28 11:15:54 -0300

    Terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -236,7 +236,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  var cambio= euro * 1.2
+  var cambio = euro * 1.2
   return cambio
 }
 
